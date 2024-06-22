7a9cd2a610ffaea63822bf35b4cf2525f1c0c6e0
1kivanc <kivanc0556@gmail.com>
2024-06-22 16:37:59 +0300

    react toastify kuruldu

--- kitapMagza/front-end/src/App.jsx
+++ kitapMagza/front-end/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import Main from './components/Main'
 import { BooksContext, ThemeContext } from './content'
 import { cartReducer, initialState } from './reducers/CartReducers'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   const [darkMode , setDarkMode] = useState(true)
@@ -11,6 +14,7 @@ function App() {
     <ThemeContext.Provider value={{darkMode,setDarkMode}}>
      <BooksContext.Provider value={{state,dispatch}}>
           <Main/>
+          <ToastContainer/>
      </BooksContext.Provider>
      
     </ThemeContext.Provider>
