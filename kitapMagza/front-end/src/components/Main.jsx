88f62fdf6c89b8d6917bc521b93c819810fb5e96
1kivanc <kivanc0556@gmail.com>
2024-06-23 10:30:26 +0300

    dark mode

--- kitapMagza/front-end/src/components/Main.jsx
+++ kitapMagza/front-end/src/components/Main.jsx
@@ -1,12 +1,20 @@
+import { useContext } from "react"
 import { Outlet } from "react-router-dom"
+import { ThemeContext } from "../content"
 
 const Main = () => {
+    const {darkMode } = useContext(ThemeContext);
   return (
-    <div>
-        <main>
+    <div className={`h-full w-full ${darkMode ? "dark" : ""}`}>
+    <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <div>Yan Panel</div>
+            <div className="min-h-screen">
             <Outlet/>
-        </main>
-    </div>
+            </div>
+        </div>
+    </main>
+</div>
   )
 }
 
